8c64966bdad5595b072381aa1e96ac082855bce0
Muhammad ali <mukhammad@appliedlabs.io>
2020-01-17 17:34:47 +0500

    updated bridge android

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { NativeModules } from 'react-native';
 
 const { RNAuth0Guardian, RNAuth0GuardianAndroid } = NativeModules;
 
+export RNAuth0GuardianAndroid;
+
 export default RNAuth0Guardian;
 
-export RNAuth0GuardianAndroid;
